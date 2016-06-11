191f66c2394e17a4f86748e64cb2a72c02451cde
Anthony Eden <me@anthonyeden.com>
2016-06-11 22:38:04 +0200

    Ensure the repo is made available in the config

--- config/config.exs
+++ config/config.exs
@@ -14,6 +14,8 @@ config :heroku_connector, HerokuConnector.Endpoint,
   pubsub: [name: HerokuConnector.PubSub,
            adapter: Phoenix.PubSub.PG2]
 
+config :heroku_connector, ecto_repos: [HerokuConnector.Repo]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
