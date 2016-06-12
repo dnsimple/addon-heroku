1cd317bdcf95be95e033be2641f612ecc00af406
Anthony Eden <me@anthonyeden.com>
2016-06-12 20:01:49 +0200

    Show account email if account settings are present in session.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule HerokuConnector.PageController do
   use HerokuConnector.Web, :controller
 
+  alias HerokuConnector.Account
+
   def index(conn, _params) do
-    render conn, "index.html"
+    case get_session(conn, :account_id) |> Account.get do
+      nil -> redirect conn, to: dnsimple_oauth_path(conn, :new)
+      account -> render conn, "index.html", account: account
+    end
   end
 end
