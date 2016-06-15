90e5b603700672796a7607fd84cbeafdcf10586b
Anthony Eden <me@anthonyeden.com>
2016-06-15 08:02:38 +0200

    Fix missing commas in config

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :heroku_connector, HerokuConnector.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("APP_HOSTNAME"), port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :logger, level: :info
@@ -17,5 +17,5 @@ config :heroku_connector, HerokuConnector.Repo,
 config :heroku_connector,
   dnsimple_client_id: System.get_env("DNSIMPLE_CLIENT_ID"),
   dnsimple_client_secret: System.get_env("DNSIMPLE_CLIENT_SECRET"),
-  heroku_client_id: System.get_env("HEROKU_CLIENT_ID")
+  heroku_client_id: System.get_env("HEROKU_CLIENT_ID"),
   heroku_client_secret: System.get_env("HEROKU_CLIENT_SECRET")
