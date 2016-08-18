d201ba2ce8d8622937e917368bf43f332927f4a6
Anthony Eden <me@anthonyeden.com>
2016-08-18 16:19:25 -0500

    Working on handling web hooks and doing something useful

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -1,16 +1,36 @@
 defmodule HerokuConnector.WebhookController do
   use HerokuConnector.Web, :controller
 
+  require Logger
+
   @doc """
   Handle inbound webhook messages.
   """
-  def handle(conn, %{"name" => name, "request_identifier" => request_identifier, "data" => _data, "account_id" => _account_id}) do
-    require Logger
+  def handle(conn, %{"name" => name, "request_identifier" => request_identifier, "data" => data, "account_id" => _account_id}) do
     HerokuConnector.WebhookRequestTracker.handle(request_identifier, fn() ->
+      case name do
+        "certificate.issue" ->
+          install_certificate(data)
+        "certificate.reissue" ->
+          install_certificate(data)
+        _ -> :ok
+      end
       Logger.info("Processing webhook #{name}")
     end)
 
     conn
     |> send_resp(200, "")
+  end
+
+  def handle(conn, _params) do
+    conn
+    |> send_resp(200, "")
+  end
+
+  defp install_certificate(data) do
+    # If the account has a connection with the certificate name
+    # And the connection has a Heroku SSL endpoint
+    # Use https://devcenter.heroku.com/articles/platform-api-reference#ssl-endpoint-update to update the cert
+    Logger.info("Installing certificate #{inspect data}")
   end
 end
